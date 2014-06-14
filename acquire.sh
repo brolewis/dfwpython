d9becced4042d5b1722008a3582bb31ddaf5c293
Lewis Franklin <lewis.franklin@gmail.com>
2014-06-14 03:07:53 -0500

    Moving crawler outside of portia

--- acquire.sh
+++ acquire.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl http://s3.amazonaws.com/wikia_xml_dumps/e/en/enmemoryalpha_pages_current.xml.gz | gunzip > memory_alpha.xml
-portiacrawl ~/Development/dfwpython/portia/slyd/data/projects/star_trek/ wikipedia -o wikipedia.json -t json
+portiacrawl ~/Development/dfwpython/crawlers/star_trek/ wikipedia -o wikipedia.json -t json
